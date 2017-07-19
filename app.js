50b24d7d97064a90d03e48fa06d0e53394ce3636
Christian Zöller <noex_home@yahoo.de>
2017-07-19 16:31:46 +0200

    Added retry logic and some delay
    as requested

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-require('dns').lookup('iana.org', (err, address, family) => {
+const dns = require('dns');
+
+setInterval(function() {
+    dns.lookup('iana.org', (err, address, family) => {
    if (err) {
      console.log("Not found");
   } else {
      console.log("Found");
   }
 });
+}, 3000);
